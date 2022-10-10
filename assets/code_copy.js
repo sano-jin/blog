4f3870cf3cc8b8deabc33781d88dfe4e56dc6af1
sano <sano@ueda.info.waseda.ac.jp>
2022-10-10 16:17:13 +0900

    updated

--- assets/code_copy.js
+++ assets/code_copy.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-  // const codeBlocks = document.querySelectorAll("pre");
   const codeBlocks = document.querySelectorAll("div.highlight");
   console.log(codeBlocks);
 
@@ -23,23 +22,4 @@ window.onload = () => {
       }, 2000);
     });
   });
-
-  // // copyCodeButton.classList.add("fa-check");
-  // // const code = codeBlocks[index].innerText;
-
-  // const copyCodeButtons = document.querySelectorAll(".copy-code-button");
-  // copyCodeButtons.forEach((copyCodeButton, index) => {
-  //   const code = codeBlocks[index].innerText;
-
-  //   copyCodeButton.addEventListener("click", () => {
-  //     window.navigator.clipboard.writeText(code);
-  //     copyCodeButton.classList.remove("fa-copy");
-  //     copyCodeButton.classList.add("fa-check");
-
-  //     setTimeout(() => {
-  //       copyCodeButton.classList.remove("fa-check");
-  //       copyCodeButton.classList.add("fa-copy");
-  //     }, 2000);
-  //   });
-  // });
 };
